5a75d951527d2338b784fdbda53d3670b2bc895b
DiogoAlvesOliveira <diogo.dream89@gmail.com>
2024-04-23 22:58:17 -0300

    defined structure

---
+++ src/main/java/com/diogo/vivo/services/impl/DbServiceImpl.java
@@ -0,0 +1,8 @@
+package com.diogo.vivo.services.impl;
+
+import com.diogo.vivo.services.DbService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DbServiceImpl implements DbService {
+}
---
+++ src/main/java/com/diogo/vivo/services/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.diogo.vivo.services.impl;
+
+import com.diogo.vivo.services.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
